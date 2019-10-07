b8df509a1fbc7b0a7d3d8086e1834a84d4bf4b5f
burovtsov <burovcov@gmail.com>
2019-10-07 10:52:45 +0300

    made with FileVisitor, Copier deleted

--- src/main/java/ExceptionLogger.java
+++ src/main/java/ExceptionLogger.java
@@ -1,18 +1,4 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
+package PACKAGE_NAME;
 
 public class ExceptionLogger {
-
-    public static void logIt(Exception e) {
-
-        Logger LOG = LogManager.getLogger(Main.class);
-
-        StringWriter stackTrace = new StringWriter();
-        e.printStackTrace(new PrintWriter(stackTrace));
-        LOG.error(e.toString() + "\nSTACK TRACE:\n" + stackTrace.toString());
-        System.out.println("Произошла ошибка. Подробности в logs/exceptions.log");
-    }
 }
